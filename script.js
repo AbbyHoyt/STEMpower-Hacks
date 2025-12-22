2800cc401785a6760eaf4bf8e934fa5b7f5b2eac
AbbyHoyt <abby.hoyt@comcast.net>
2025-12-21 18:17:37 -0800

    Fixed message generator display and copy.

--- script.js
+++ script.js
@@ -6,34 +6,31 @@ if (scriptButton) {
 }
 
 function generateScript() {
-  var recipient = document.getElementById("recipient-name").value;
-  var sender = document.getElementById("your-name").value;
-  var issue = document.getElementById("issue").value;
+    var recipient = document.getElementById("recipient-name").value;
+    var sender = document.getElementById("your-name").value;
+    var issue = document.getElementById("issue").value;
+
+    displayScript.innerHTML = "Hi " + recipient + " it's " + sender + ". Remember that your vote matters! Voting is important because of issues like " + issue + ".";
+    displayScript = "Hi " + recipient + " it's " + sender + ". Remember that your vote matters! Voting is important because of issues like " + issue + ".";
 }
-
-displayScript.innerHTML = "Hi " + recipient + " it's " + sender + ". Remember that your vote matters! Voting is important because of issues like " + issue + ".";
-
-displayScript = "Hi " + recipient + " it's " + sender + ". Remember that your vote matters! Voting is important because of issues like " + issue + ".";
 
 /*Declare Copy Button Variable */
 var copyButton = document.getElementById("copy-button");
 
 /* Store Event Listener In Case the DOM Does Not Load Fully */
 if (copyButton) {
-  copyButton.addEventListener("click", copyScriptText);
+    copyButton.addEventListener("click", copyScriptText);
 }
 
 /* Copy Script Text Function */
 function copyScriptText() {
-  var str = document.getElementById("display-script");
-  var el = document.createElement("textarea");
-  el.value = str;
+    var str = document.getElementById("script-element");
+    var el = document.createElement("textarea");
+    el.value = str;
 
-  el.select();
-  el.setSelectionRange(0, 99999);
-
-  navigator.clipboard.writeText(displayScript);
-
-/*  alert("Successfully Copied the Text: "  + displayScript); */
-  alert("Personalized message successfully copied to clipboard!");
+    el.select();
+    el.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(displayScript);
+    
+    alert("Personalized message successfully copied to clipboard!");
 }
