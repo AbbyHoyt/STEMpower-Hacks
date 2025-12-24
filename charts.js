1fc1b2ecb668f544179b88303e89063958cf50e6
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-12-23 20:57:16 -0800

    Added voter turnout pie charts.

--- charts.js
+++ charts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     Highcharts.setOptions({
-        colors: ['#093FB4', '#FFD8D8', '#ED3500'],
+        colors: ['#FFD8D8', '#ED3500', '#093FB4'],
     });
 
     const localGovRepresentation = Highcharts.chart('localGovRepresentation', {
@@ -41,5 +41,129 @@ document.addEventListener('DOMContentLoaded', function () {
                    { name: 'Western Asia and Northern Africa', y: 2 }
                   ]
         }]
+    });
+
+    const voterTurnoutMen = Highcharts.chart('voterTurnoutMen', {
+        chart: {
+            type: 'pie',
+            backgroundColor: '#FFFCFB', 
+            plotBackgroundColor: '#FFFCFB', 
+            zooming: {
+                type: 'xy'
+            },
+            panning: {
+                enabled: true,
+                type: 'xy'
+            },
+            panKey: 'shift'
+        },
+        title: {
+            text: 'Proportion of Eligible Adult Population Who Reported Voting (Men, 2024)',
+        },
+        tooltip: {
+            valueSuffix: '%'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: [{
+                    enabled: true,
+                    distance: 20
+                }, {
+                    enabled: true,
+                    distance: -40,
+                    format: '{point.percentage:.1f}%',
+                    style: {
+                        fontSize: '1.2em',
+                        textOutline: 'none',
+                        opacity: 0.7
+                    },
+                    filter: {
+                        operator: '>',
+                        property: 'percentage',
+                        value: 10
+                    }
+                }]
+            }
+        },
+        series: [
+            {
+                name: 'Percentage',
+                colorByPoint: true,
+                data: [
+                    {
+                        name: 'Voted',
+                        y: 63.7
+                    },
+                    {
+                        name: 'Did Not Vote',
+                        y: 36.3
+                    },
+                ]
+            }
+        ]
+    });
+
+    const voterTurnoutWomen = Highcharts.chart('voterTurnoutWomen', {
+        chart: {
+            type: 'pie',
+            backgroundColor: '#FFFCFB', 
+            plotBackgroundColor: '#FFFCFB',
+            zooming: {
+                type: 'xy'
+            },
+            panning: {
+                enabled: true,
+                type: 'xy'
+            },
+            panKey: 'shift'
+        },
+        title: {
+            text: 'Proportion of Eligible Adult Population Who Reported Voting (Women, 2024)',
+        },
+        tooltip: {
+            valueSuffix: '%'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: [{
+                    enabled: true,
+                    distance: 20
+                }, {
+                    enabled: true,
+                    distance: -40,
+                    format: '{point.percentage:.1f}%',
+                    style: {
+                        fontSize: '1.2em',
+                        textOutline: 'none',
+                        opacity: 0.7
+                    },
+                    filter: {
+                        operator: '>',
+                        property: 'percentage',
+                        value: 10
+                    }
+                }]
+            }
+        },
+        series: [
+            {
+                name: 'Percentage',
+                colorByPoint: true,
+                data: [
+                    {
+                        name: 'Voted',
+                        y: 66.9
+                    },
+                    {
+                        name: 'Did Not Vote',
+                        y: 33.1
+                    },
+                ]
+            }
+        ]
     });
 });
