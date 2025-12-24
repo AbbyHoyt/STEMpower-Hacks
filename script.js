a00bc05c0382d8ac06a0a7c9dd7105dd57e69af2
AbbyHoyt <abby.hoyt@comcast.net>
2025-12-23 23:10:39 -0800

    Fixed quotes, turned off user select for a bunch of elements.

--- script.js
+++ script.js
@@ -37,14 +37,24 @@ function copyScriptText() {
 }
 
 var quoteList = [
-    "\"Women belong in all places where decisions are being made. It shouldn't be that women are the exception.\" -Ruth Bader Ginsburg",
-    "\"The only people who know what girls want are girls.\" -Malala Yousafzai",
-    "\"Any great change must expect opposition, because it shakes the very foundation of privilege.\" -Lucretia Mott",
-    "\"I know nothing of man's rights, or woman's rights; human rights are all that I recognize.\" -Sarah Moore Grimke",
-    "\"Failure is impossible.\" -Susan B. Anthony"
+    "\"Women belong in all places where decisions are being made. It shouldn't be that women are the exception.\"",
+    "\"The only people who know what girls want are girls.\"",
+    "\"Any great change must expect opposition, because it shakes the very foundation of privilege.\"",
+    "\"I know nothing of man's rights, or woman's rights; human rights are all that I recognize.\"",
+    "\"Failure is impossible.\""
+]
+
+var authorList = [
+    "-Ruth Bader Ginsburg",
+    "-Malala Yousafzai",
+    "-Lucretia Mott",
+    "-Sarah Moore Grimke",
+    "-Susan B. Anthony"
 ]
 
 var quote = document.getElementById("quote");
+
+var author = document.getElementById("author")
 
 var button = document.getElementById("inspire-me");
 
@@ -54,7 +64,10 @@ button.addEventListener("click", displayQuote);
 
 function displayQuote() {
     quote.innerHTML = quoteList[count];
+    author.innerHTML = authorList[count];
+
     count++;
+
     if (count == quoteList.length) {
         count = 0;
     }
