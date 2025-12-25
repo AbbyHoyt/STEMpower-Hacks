3edfa1d477fd2c87c608a6e446a2d87946aaa940
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-12-25 00:15:52 -0800

    Added 19th Amendment typewriter text.

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+var i = 0;
+var text = 'The right of citizens of the United States to vote shall not be denied or abridged by the United States or by any State on account of sex.';
+var speed = 40;
+
+function typewriter() {
+  if (i < text.length) { 
+    document.getElementById("typewriterText").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typewriter, speed);
+  }
+}
